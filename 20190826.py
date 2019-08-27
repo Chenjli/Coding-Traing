350fa848c3637518017bfd20fdc04139b264af65
Chenjli <54526097+Chenjli@users.noreply.github.com>
2019-08-27 19:15:25 +0800

    Complete

--- 20190826.py
+++ 20190826.py
@@ -1,21 +1,21 @@
-print("***********Exercise 1*************\n")
+print("*************Exercise 1*************")
 print("I'm a CUHK msC IN FinTech student.\nHello everybody in this course!\nLet's solve problems by programming in Python.")
 
-print("\n***********Exercise 2*************\n")
+print("\n***********Exercise 2*************")
 a = int(input("Input 1:"))
 b = int(input("Input 2:"))
 c = int(input("Input 3:"))
 d = int(input("Input 4:"))
 print("The minimum value is ", min(a,b,c,d),"whereas the maximum value is ", max(a,b,c,d))
 
-print("\n***********Exercise 3*************\n")
+print("\n***********Exercise 3*************")
 minutes = int(input("Please input minutes:"))
 D = minutes // (24 * 60)
 H = (minutes % (24 * 60)) // 60
 M = (minutes % (24 * 60)) % 60
 print(D,"Days",H,"Hours",M,"Minutes")
 
-print("\n***********Exercise 4*************\n")
+print("\n***********Exercise 4*************")
 import getpass
 print("Player 1, write down your number secretly:")
 num = int(getpass.getpass())
